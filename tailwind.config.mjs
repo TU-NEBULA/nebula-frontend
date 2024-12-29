eda78b90deec1e9e0f131e7a504fd62b4e5d2b62
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-12-29 14:17:44 +0900

    chore: 컬러 디자인 시스템 적용

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#111111",
+        grey1: "#F5F5F5",
+        grey2: "#F9FAFB",
+        white: "#FFFFFF",
+      },
+    },
   },
 };

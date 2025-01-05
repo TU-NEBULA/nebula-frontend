ed0e8aa32cdf4a75b6b7bb56ed6e946e2fb0243c
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-06 02:42:25 +0900

    refactor: as prop 추가

--- packages/ui/src/flex/index.tsx
+++ packages/ui/src/flex/index.tsx
@@ -1,21 +1,37 @@
-interface FlexProps {
-  children: React.ReactNode;
-  direction: "row" | "row-reverse" | "col" | "col-reverse";
-  justify: "start" | "end" | "center" | "between" | "around" | "evenly";
-  align: "start" | "end" | "center" | "stretch" | "baseline";
-  wrap: "nowrap" | "wrap" | "wrap-reverse";
-  className: string;
+type Direction = "row" | "row-reverse" | "col" | "col-reverse";
+type Justify = "start" | "end" | "center" | "between" | "around" | "evenly";
+type Align = "start" | "end" | "center" | "stretch" | "baseline";
+type Wrap = "nowrap" | "wrap" | "wrap-reverse";
+
+interface FlexOwnProps {
+  direction?: Direction;
+  justify?: Justify;
+  align?: Align;
+  wrap?: Wrap;
+  className?: string;
 }
 
-export default function Flex({
+type FlexProps<T extends React.ElementType> = FlexOwnProps &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof FlexOwnProps> & {
+    as?: T;
+  };
+
+export default function Flex<T extends React.ElementType = "div">({
+  as,
   direction = "row",
   justify = "start",
   align = "start",
   wrap = "nowrap",
   className = "",
   children,
-}: Partial<FlexProps>) {
+  ...restProps
+}: FlexProps<T>) {
+  const Component = as || "div";
   const flexClass = `flex flex-${direction} flex-${wrap} justify-${justify} items-${align}`;
 
-  return <div className={`${flexClass} ${className}`}>{children}</div>;
+  return (
+    <Component className={`${flexClass} ${className}`} {...restProps}>
+      {children}
+    </Component>
+  );
 }

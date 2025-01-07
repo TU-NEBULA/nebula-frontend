83c9ba7bb097ed01b51038261b988ef19f2cf4b2
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-08 02:03:39 +0900

    feat: 북마크 페이지 라우팅 설정

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout";
 import Agreement from "./pages/agreement";
+import Bookmark from "./pages/bookmark";
 import Home from "./pages/home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/agreement" element={<Agreement />} />
+          <Route path="/bookmark" element={<Bookmark />} />
         </Route>
       </Routes>
     </Router>

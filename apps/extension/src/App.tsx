835c30f4336af0d5849d6348be597e0695017e88
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-10 15:16:42 +0900

    feat: 북마크 생성 페이지 라우팅 설정

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import Agreement from "./pages/agreement";
 import Bookmark from "./pages/bookmark";
+import CreateBookmark from "./pages/create-bookmark";
 import Home from "./pages/home";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/agreement" element={<Agreement />} />
           <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/create-bookmark" element={<CreateBookmark />} />
         </Route>
       </Routes>
     </Router>
---
+++ apps/extension/src/pages/create-bookmark.tsx
@@ -0,0 +1,5 @@
+const CreateBookmark = () => {
+  return <div>CreateBookmark</div>;
+};
+
+export default CreateBookmark;

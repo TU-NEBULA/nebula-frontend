aeb9af2aeab038dad55449987b1fcb476dfa74d4
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-02-15 21:46:21 +0900

    feat: 로그인 API 연동

--- apps/www/src/app/redirect/page.tsx
+++ apps/www/src/app/redirect/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import { setupAuth } from "@/utils/cookies";
 import { Spinner } from "@repo/ui";
 
 const RedirectPage = () => {
+  const router = useRouter();
+
   useEffect(() => {
-    setupAuth();
+    (async () => {
+      const result = await setupAuth();
+      if (!result) alert("잘못된 접근입니다.");
+      router.replace("/");
+    })();
   }, []);
 
   return (
--- apps/www/src/components/layout/header/index.tsx
+++ apps/www/src/components/layout/header/index.tsx
@@ -1,16 +1,22 @@
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 
 import logo from "@/assets/icons/logo.svg";
 
-const Header = () => {
+import LoginButton from "./login-button";
+
+const Header = async () => {
+  const cookieStorage = await cookies();
+  const accessToken = cookieStorage.get("accessToken");
+
   return (
     <header className="text-white py-6 px-10 flex justify-between">
       <Link href="/" className="flex items-center gap-2">
         <Image src={logo} alt="nebula 로고" width={40} height={20} />
         <p className="text-xl font-semibold">Nebula</p>
       </Link>
-      <Link href="/login">Login</Link>
+      <LoginButton token={accessToken?.value || null} />
     </header>
   );
 };

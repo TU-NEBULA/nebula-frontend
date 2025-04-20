0c0308ab04fbe206c4f86d1b8c26e2e35c8a6953
gs0428 <lider28@naver.com>
2025-04-20 23:19:14 +0900

    fix: 로그아웃 시 최신화 되도록 수정

--- apps/www/src/components/layout/header/login-button.tsx
+++ apps/www/src/components/layout/header/login-button.tsx
@@ -17,6 +17,7 @@ const LoginButton = ({ token }: LoginButtonProps) => {
       if (res.ok) {
         alert("로그아웃 되었습니다.");
         router.replace("/");
+        router.refresh();
       }
     } else {
       router.push("/login");

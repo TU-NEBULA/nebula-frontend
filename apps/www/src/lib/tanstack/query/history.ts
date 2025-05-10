313e3e26be91ebd487146a216abb23016fd27266
gs0428 <lider28@naver.com>
2025-05-11 03:12:33 +0900

    feat: 방문기록 검색 API 연동

--- apps/www/src/lib/tanstack/query/history.ts
+++ apps/www/src/lib/tanstack/query/history.ts
@@ -2,9 +2,9 @@ import { HISTORY } from "@/constants/history";
 import { getHistories } from "@/service/history";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetHistories = (page: string) => {
+export const useGetHistories = ({ page, keyword }: { page: string; keyword: string }) => {
   return useQuery({
-    queryKey: [HISTORY.GET_HISTORIES, page],
-    queryFn: () => getHistories(page),
+    queryKey: [HISTORY.GET_HISTORIES, page, keyword],
+    queryFn: () => getHistories({ page, keyword }),
   });
 };

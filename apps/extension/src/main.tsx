c782126c239cd14a82852c8982ed8156b0c45e57
gs0428 <lider28@naver.com>
2025-06-08 12:12:07 +0900

    chore: stagewise 세팅

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -1,7 +1,15 @@
 import "./index.css";
 
+import { StagewiseToolbar } from "@stagewise/toolbar-react";
+
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
+
+if (import.meta.env.DEV) {
+  const toolbarRoot = createRoot(document.createElement("div"));
+  toolbarRoot.render(<StagewiseToolbar config={{ plugins: [] }} />);
+}
